bc4271fc0adc6b9251cc38e13becb4defccfd35c
Maximiliano <calderonmaxi@outlook.com>
2022-02-24 20:00:41 -0300

    update: revise bootstrap styles and splide slider

--- slider.js
+++ slider.js
@@ -1,7 +1,2 @@
-const splide = new Splide( '.splide', {
-    type   : 'loop',
-    perPage: 3,
-    perMove: 1,
-  } );
-  
-  splide.mount();
+var splide = new Splide( '.splide' , {type:"loop"});
+splide.mount();
